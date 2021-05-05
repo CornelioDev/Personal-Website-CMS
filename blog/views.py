449f9cb0a15555e444cf9553464a2637fa1e4e4f
José Manuel Cornelio <batcornelio@gmail.com>
2021-05-05 12:44:09 -0400

    404 in Categories

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Article, Category
 
 # Create your views here.
@@ -12,7 +12,7 @@ def list(request):
     })
 
 def category(request, category_id):
-    category = Category.objects.get(id=category_id)
+    category = get_object_or_404(Category, id=category_id)
 
     return render(request, 'categories/category.html', {
         'title':category,
