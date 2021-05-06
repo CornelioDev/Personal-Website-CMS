de3929591b4ac40ee7c6f00fa3d92ffc599f0c01
José Manuel Cornelio <batcornelio@gmail.com>
2021-05-06 13:31:19 -0400

    Articles pagination

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from .models import Article, Category
 
 # Create your views here.
 def list(request):
+    # Sacar artículos
+    articles = Article.objects.filter(public='True')
 
-    articles = Article.objects.all()
+    # Paginar artículos
+    paginator = Paginator(articles, 4)
+
+    # Recoger número de página
+    page = request.GET.get('page')
+    page_articles = paginator.get_page(page)
 
     return render(request, 'articles/list.html', {
         'title':'Todos los artículos',
-        'articles':articles
+        'articles':page_articles
     })
 
 def category(request, category_id):
