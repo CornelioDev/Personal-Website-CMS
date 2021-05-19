52c0ee50a14fe64e61bb6344cf9ce77db29629c1
Jose Manuel Cornelio <batcornelio@gmail.com>
2021-05-19 14:59:20 -0400

    User login

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from mainapp.forms import RegisterForm
 # Create your views here.
 
@@ -28,4 +29,21 @@ def register_page(request):
     return render(request, 'users/register.html', {
         'title': 'Registro',
         'register_form': register_form
+    })
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.warning(request, 'No te has identificado correctamente :(')
+    return render(request, 'users/login.html', {
+        'title': 'Iniciar Sesión'
     })
